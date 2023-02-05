4de40071442946ccbde403b194b20cfae151be05
R. C. van der Zande <rz@warpnet.nl>
2023-02-05 20:18:16 +0100

    make random table setup choice at begin of the game

--- vpool.py
+++ vpool.py
@@ -1,3 +1,4 @@
+from random import choice
 from vpython import *
 
 
@@ -20,8 +21,9 @@ class Prog:
         self.caption = Caption()
         self.camera = Camera()
 
-        # setting up minimal components to make the keydown_func funtion.
-        self.game = Game(self.rate, self.settings["ball_size"], self.table, self.caption, self.games_settings['Libre'], self.players, self.object_score)
+        # setting up table for menu animation.
+        set_up_choice = choice([self.games_settings['Libre'], self.games_settings['Hundred'], self.games_settings['Over red']])
+        self.game = Game(self.rate, self.settings["ball_size"], self.table, self.caption, set_up_choice, self.players, self.object_score)
         self.game.cue.invisible()
 
 
@@ -62,6 +64,14 @@ class Prog:
         if self.easy_mode:
             self.object_score = 3
         self.game = Game(self.rate, self.settings["ball_size"], self.table, self.caption, self.games_settings['Libre'], self.players, self.object_score)
+
+    def set_up_three_cushion(self):
+        """set caption and game specific settings for Libre"""
+        self.setup_game()
+        self.caption.set_three_cushion()
+        if self.easy_mode:
+            self.object_score = 3
+        self.game = Game(self.rate, self.settings["ball_size"], self.table, self.caption, self.games_settings['Three cushion'], self.players, self.object_score)
 
     def set_up_hundred(self):
         """set caption and game specific settings for Hundred"""
@@ -604,6 +614,59 @@ class LibreScore(Score):
             return True
         return False
 
+class ThreeCushionScore(LibreScore):
+
+    def __init__(self, players, object_score):
+        """Set up score for the Libre game."""
+        super().__init__()
+        self.players = players
+        self.score = {player: 0 for player in players}
+        self.turn = 1
+        self.object_score = 20 if object_score == None else object_score
+
+    def __repr__(self):
+        """Set respresentation of instance of LibreScore"""
+        s = "Current score: "
+        for player in self.players:
+            s += f"{player}: {self.score[player]}, "
+        s += f"turn: {self.turn}"
+        return s
+
+    def victor(self):
+        """Determine the winner"""
+        s = str(self)
+        victors = [key for key, value in self.score.items() if value == max(self.score.values())]
+        if len(victors) > 1:
+            return f"{s}\nWinnaars: {', '.join(victors)}\n"
+        return f"{s}\nWinnaar: {victors[0]}\n"
+
+    def next_turn(self):
+        """Increase turn marker by 1."""
+        self.turn += 1
+
+    def get_turn(self):
+        """Return number of turns taken."""
+        return self.turn
+
+    def get_player_score(self, player):
+        """Return the score of a player"""
+        return self.score[player]
+
+    def score_shot(self, player, objectives, collisions):
+        """Determine if a player scored points based on collisions."""
+        for objective in objectives:
+            if self.hit_objective(objective["Combination"], collisions):
+                self.score[player] = self.score[player] + objective["Points"]
+                return True
+        return False
+
+    def check_victor(self):
+        """Check if there is a winner."""
+        if self.get_turn() == self.object_score:
+            return True
+        return False
+
+
 class HundredScore(LibreScore):
 
     def __init__(self, players, object_score):
@@ -701,14 +764,19 @@ class Caption:
         self.game = self.explain_libre()
 
     def set_hundred(self):
-        """Select text for Libre game"""
+        """Select text for Hundred game"""
         self.interface = self.explain_interface()
         self.game = self.explain_hundred()
 
     def set_over_red(self):
-        """Select text for Libre game"""
+        """Select text for 10 over Red game"""
         self.interface = self.explain_interface()
         self.game = self.explain_over_red()
+
+    def set_three_cushion(self):
+        """Select text for Three Cushion game"""
+        self.interface = self.explain_interface()
+        self.game = self.explain_three_cushion()
 
     def set_menu(self):
         """Select text for menu"""
@@ -724,6 +792,14 @@ class Caption:
 Game rules:
 Players take turns and try to hit the other two balls with their cueball. Making this carambool will result in a point. The player with the most points after 20
 turns wil win the game. (Easy mode = 3 turns)
+"""
+
+    def explain_three_cushion(self):
+        """Explanation for Three Cushion"""
+        return  """
+Game rules:
+Players take turns and try to hit the other two balls with their cueball, however three cushions need to be hit before hitting the final ball. Making this carambool 
+will result in a point. The player with the most points after 20 turns wil win the game. (Easy mode = 3 turns)
 """
 
     def explain_hundred(self):
@@ -748,9 +824,9 @@ than 7. Game wil stop if a player hits 10 points, there will be an equalizing tu
 Interface explaination:
 The 'cue' give the direction and power of the shot. The angle can be adjusted clockwise: 'D' - 0.1, 'd' - 1, 'e' - 10, 'E' - 90 degrees;
 or counterclockwise: 'A' - 0.1, 'a' - 1, 'q' - 10, 'Q' - 90 degrees. The power of the shot can be adjusted by: 'w' for increase or 's' for 
-decrease, 'W' and 'S' will do a 10 fold jump. To take a shot press 'space bar'. 
-The cueball can be stopped by pressing 'z' or 'x' for all balls.
-Right click on the mouse + moving will move the camera.
+decrease, 'W' and 'S' will do a 10 fold jump. To take a shot press 'space bar'. The cueball can be stopped by pressing 'z' or 'x' for all 
+balls. Right click on the mouse + moving will move the camera. The arrows wil move the camera around. Movement is relative to the in game 
+coordinate system, not to the viewpoint of the observer. 'p' will quit the game.
 """
 
     def explain_menu(self):
@@ -794,9 +870,8 @@ def keydown_func(evt):
             '1': {'bools': [prog.menu_bool], 'func': prog.set_up_libre, 'args': ()},
             '2': {'bools': [prog.menu_bool], 'func': prog.set_up_hundred, 'args': ()},
             '3': {'bools': [prog.menu_bool], 'func': prog.set_up_over_red, 'args': ()},
-            # '4': {'bools': [prog.menu_bool], 'func': prog.game.create_libre, 'args': ()},  
+            '4': {'bools': [prog.menu_bool], 'func': prog.set_up_three_cushion, 'args': ()},  
 
-            # 'p': {'bools': [True], 'func': game.quit, 'args': ()}, 
         }
 
     key = evt.key
@@ -925,37 +1000,41 @@ over_red = {
     "score": OverRedScore
 }
 
-# three_cushion = {
-#     "balls": {
-#         "cueballs": [True, False, False],
-#         "colors": [
-#             {"color": "WHITE", "vector": vector(255/255, 255/255, 255/255)},
-#             {"color": "YELLOW", "vector": vector(255/255, 255/255, 0)},
-#             {"color": "RED", "vector": vector(255/255, 0, 0)},
-#         ],
-#         "start_locations": [
-#             vector(-settings["table"]["height"] // 4, settings["ball_size"], 0),
-#             vector(0, settings["ball_size"], 0),
-#             vector(settings["table"]["height"] // 4, settings["ball_size"], 0),
-#         ],
-#     },
-#     "goals": [
-#         {
-#             "Cueball": "WHITE", 
-#             "Combinations": [
-#                 {
-#                     "Combination": ["RED", "YELLOW"], 
-#                     "Points": 1
-#                 },
-#                 {
-#                     "Combination": ["RED"], 
-#                     "Points": 0
-#                 },
-#             ],
-#         }
-#     ],
-#     "score": OverRedScore
-# }
+three_cushion = {
+    "balls": {
+        "cueballs": [True, True, False],
+        "colors": [
+            {"color": "WHITE", "vector": vector(255/255, 255/255, 255/255)},
+            {"color": "YELLOW", "vector": vector(255/255, 255/255, 0)},
+            {"color": "RED", "vector": vector(255/255, 0, 0)},
+        ],
+        "start_locations": [
+            vector(-settings["table"]["height"] // 4, settings["ball_size"], settings["table"]["acquit"]),
+            vector(-settings["table"]["height"] // 4, settings["ball_size"], 0),
+            vector(settings["table"]["height"] // 4, settings["ball_size"], 0),
+        ],
+    },
+    "goals": [
+        {
+            "Cueball": "WHITE", 
+            "Combinations": [
+                {
+                    "Combination": ["YELLOW", "RED"], 
+                    "Points": 1
+                },
+            ],
+        },{
+            "Cueball": "YELLOW", 
+            "Combinations": [
+                {
+                    "Combination": ["WHITE", "RED"], 
+                    "Points": 1
+                },
+            ],
+        },
+    ],
+    "score": ThreeCushionScore
+}
 
 
 if __name__ == '__main__':
@@ -976,7 +1055,7 @@ if __name__ == '__main__':
         'Libre': libre,
         'Hundred': hundred,
         'Over red': over_red,
-        # '3-banden': three_cushion,
+        'Three cushion': three_cushion,
     }
 
     players = ["Player 1", "Player 2"]
